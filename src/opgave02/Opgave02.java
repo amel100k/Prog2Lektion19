1264bb5179bc01d99adb4ace78f77168dac934dc
amel100k <eaa23ema@students.eaaa.dk>
2024-04-15 12:01:56 +0200

    opgave01 og opgave02

--- src/opgave02/Opgave02.java
+++ src/opgave02/Opgave02.java
@@ -8,11 +8,14 @@ public class Opgave02 {
     public static void main(String[] args) {
         Customer student = new Customer(new StudentDiscount());
         Customer worker = new Customer(new WorkerDiscount());
+        Customer normal = new Customer(new NormalDiscount());
         student.placeOrder(new Order(2, new Beer()));
         student.placeOrder(new Order(1, new PepsiMax()));
         student.placeOrder(new Order(3, new Beer()));
         worker.placeOrder(new Order(10, new Beer()));
+        normal.placeOrder(new Order(10, new Beer()));
         student.getBarTab().Print();
         worker.getBarTab().Print();
+        normal.getBarTab().Print();
     }
 }
